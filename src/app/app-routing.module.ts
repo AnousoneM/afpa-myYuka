2919ed50954901d0af24292cb9c11a718fdf4f77
AnousoneM <anousone.mounivongs@gmail.com>
2023-05-31 15:08:01 +0200

    mise en place du component Home + Module Http et reactiveForm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// importation du composant Home
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Définition de la route par défaut
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+// import du module pour les formulaires
+import { ReactiveFormsModule } from '@angular/forms';
+// import du module pour les requêtes HTTP
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    // ajout du module pour les formulaires
+    ReactiveFormsModule,
+    // ajout du module pour les requêtes HTTP
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
